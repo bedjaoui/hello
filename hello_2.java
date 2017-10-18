64f36359f88def97e26d34464eb26644a361557a
Bedjaoui <djounaydi.bedjaoui@cgi.com>
2017-10-18 16:12:43 +0200

    add hello_2.java

---
+++ hello_2.java
@@ -0,0 +1,14 @@
+public class hello_2{
+	
+	public static void main(String[] args){
+		
+		for(int i=1;i<15;i++){
+			System.out.println(" Hello  world ===> " +i+ "  test commit <==== ");
+			
+		}
+		
+	}
+	
+	
+	
+}
