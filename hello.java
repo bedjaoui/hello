0f0b4842b4270770cb3de5937d37ac8471ad6b58
Bedjaoui <djounaydi.bedjaoui@cgi.com>
2017-10-18 13:57:47 +0200

    mise a jours de hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 	public static void main(String[] args){
 		
 		for(int i=1;i<15;i++){
-			System.out.println(" Hello  world ===> " +i+ "  <==== ");
+			System.out.println(" Hello  world ===> " +i+ "  test commit <==== ");
 			
 		}
 		
